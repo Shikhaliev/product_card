1aae70b1aa5d3c54ee95c27958d8b49a194b8718
Ansar <shihalievansar@yandex.ru>
2026-07-10 02:08:35 +0300

    Добывил кнопу перехода на google

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const productsCard = document.querySelectorAll('.products__item');
 
 const productColorChangeButton = document.querySelector('.change-color-card-button');
 const productsColorChangeButton = document.querySelector('.change-color-all-card-button');
+const openGoogleButton = document.querySelector('.open-google-button');
 
 const firstColorHash = '#5150762a';
 const secondColorHash = '#130ddb2a';
@@ -16,3 +17,19 @@ productColorChangeButton .addEventListener('click', () => {
 productsColorChangeButton.addEventListener('click', () => {
     productsCard.forEach((card) => card.style.backgroundColor = secondColorHash)
     });
+
+// Переход на google
+openGoogleButton.addEventListener('click', openGoogle
+)
+
+function openGoogle(){
+    const answer = "Вы действительно хотите перейти по сылке ?";
+
+    if(answer === true){
+        window.open('https://www.google.com/?hl=ru_ru');
+    }
+    else{
+        return;
+    }
+}
+
