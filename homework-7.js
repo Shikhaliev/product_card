c58a0d836d24771b20be8f05b6f5f7a00e20fb2e
Ansar <shihalievansar@yandex.ru>
2026-07-16 03:48:38 +0300

    Исправил ошибки и добавил больше проверок функций

--- homework-7.js
+++ homework-7.js
@@ -3,7 +3,7 @@ const LIGHT_SPEED = 299792458;
 let product = "iphone 17pro Max Ultra ";
 let price = 1299;
 
-function SpeedCheck(speed) {
+function speedCheck(speed) {
     if (speed > LIGHT_SPEED) {
         console.log("Сверхсветовая скорость");
     }
@@ -15,19 +15,24 @@ function SpeedCheck(speed) {
     }
 }
 
-function CalculatCash(budget){
+function calculatCash(budget){
     if(budget > price){
         console.log(`${product} приобретён. Спасибо за покупку!`);
     } else{
-        console.log(`Вам не хватает ${+budget - price}$, пополните баланс`);
+        console.log(`Вам не хватает ${Number(budget) - price}$, пополните баланс`);
     }
 }
 
-function WeatherOut(degree, city) {
+function weatherOut(degree, city) {
     console.log(`Сейчас в ${city} температура — ${degree} градусов по Цельсию`);
 }
 
-SpeedCheck(300000);
-CalculatCash(10);
-WeatherOut(40, 'Rostov-na-Donu');
+speedCheck(300000);
+speedCheck(300000000);
+speedCheck(299792458);
+
+calculatCash(10);
+calculatCash(1500);
+
+weatherOut(40, 'Rostov-na-Donu');
 
