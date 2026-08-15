6976fcf3188a6d8b086e2f72da486b5054444fb2
Ansar <shihalievansar@yandex.ru>
2026-08-15 15:20:23 +0300

    реализоввал шаблон карт через JS, исправил некоторые файлы моменты с style

--- scripts/arrayJS/product_card_array.js
+++ scripts/arrayJS/product_card_array.js
@@ -1,4 +1,4 @@
-export const productsCard = [
+export const products = [
     {
         name: "Увлажняющий мусс",
         category: "для нормальной кожи",
--- scripts/homework-10.js
+++ scripts/homework-10.js
@@ -1,2 +1,23 @@
-const productsList = document.getElementsByClassName("products");
-const productsItem = document.getElementsByClassName("card");
+import { products } from "./arrayJS/product_card_array.js";
+
+const productsTemplate = document.querySelector('.products-template');
+const productsList = document.querySelector('.products');
+
+
+products.forEach((product) => {
+    const productsClone = productsTemplate.content.cloneNode(true);
+    productsClone.querySelector('.card__category').textContent = product.category
+    productsClone.querySelector('.card__name').textContent = product.name
+    productsClone.querySelector('.card__description').textContent = product.description
+    productsClone.querySelector('.card__price-block span').textContent = product.price + ' ₽'
+
+    const structureItems = productsClone.querySelectorAll('.compound__structure li');
+
+    structureItems.forEach((li, index) => {
+        if (index < product.structure.length) {
+            li.textContent = product.structure[index];
+        }
+    });
+    
+    productsList.appendChild(productsClone);
+})
