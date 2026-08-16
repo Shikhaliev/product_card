5ddd4bc37e96383282c0a665fa6e4dec01390a79
Ansar <shihalievansar@yandex.ru>
2026-08-16 15:31:41 +0300

    Выполнил 5 задание: вывод карточек по количесвту заданному пользоватею и проверка данного числа , также выполнено 4 задание с методом reduce

--- scripts/homework-10.js
+++ scripts/homework-10.js
@@ -3,8 +3,11 @@ import { products } from "./arrayJS/product_card_array.js";
 const productsTemplate = document.querySelector('.products-template');
 const productsList = document.querySelector('.products');
 
+const numCards = prompt("Сколько карточек отобразить? От 1 до 5");
 
-products.forEach((product) => {
+const someArray = products.slice(0, checkNumCards(numCards));
+
+someArray.forEach((product) => {
     const productsClone = productsTemplate.content.cloneNode(true);
     productsClone.querySelector('.card__category').textContent = product.category
     productsClone.querySelector('.card__name').textContent = product.name
@@ -16,8 +19,24 @@ products.forEach((product) => {
     structureItems.forEach((li, index) => {
         if (index < product.structure.length) {
             li.textContent = product.structure[index];
-        }
+        };
     });
     
     productsList.appendChild(productsClone);
-})
+});
+
+const newArray = products.reduce((acc, value) =>{
+    return {
+        ...acc,
+        [value.name]: value.description,
+    }
+}, {});
+
+function checkNumCards(num){
+    if(0 > num || num > 5){
+        console.log("Вы ввели неправильное число !")
+        return num = 0;
+    } else {
+        return num;
+    }
+};
