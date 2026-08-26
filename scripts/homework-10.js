447c8498481386559897e0a73329873f5fcf2fe5
Ansar <shihalievansar@yandex.ru>
2026-08-26 15:12:50 +0300

    Поправил ошибки №2

--- scripts/homework-10.js
+++ scripts/homework-10.js
@@ -3,9 +3,9 @@ import { products } from "./arrayJS/product_card_array.js";
 const productsTemplate = document.querySelector('.products-template');
 const productsList = document.querySelector('.products');
 
-const productsForDisplay = products.slice(0, checkNumCards());
+const productsForDisplay = products.slice(0, checkNumCards()); 
 
-displayCards();
+displayCards(productsForDisplay);
 
 const newArray = products.reduce((acc, value) => {
     return [
@@ -14,35 +14,35 @@ const newArray = products.reduce((acc, value) => {
     ]
 }, []);
 
+function displayCards(cards){
+    cards.forEach((product) => {
+        const productsClone = productsTemplate.content.cloneNode(true);
+        productsClone.querySelector('.card__category').textContent = product.category
+        productsClone.querySelector('.card__name').textContent = product.name
+        productsClone.querySelector('.card__description').textContent = product.description
+        productsClone.querySelector('.card__price-block span').textContent = product.price + ' ₽'
+        const imageElement = productsClone.querySelector('.card__image');
+        imageElement.src = product.image;
+
+        const compoundList = productsClone.querySelector('.compound__structure');
+        compoundList.innerHTML = '';
+
+        product.structure.forEach((item) => {
+            const li = document.createElement('li');
+            li.textContent = item;
+            compoundList.appendChild(li);
+        })
+
+        productsList.appendChild(productsClone);
+    })
+}
+
 function checkNumCards(){
     let сardsNum = +prompt("Сколько карточек отобразить? От 1 до 5");
-    if(isNaN(сardsNum) || 0 > сardsNum || сardsNum > 5){
+    if (isNaN(сardsNum) || 0 > сardsNum || сardsNum > 5){
         console.log("Вы ввели неправильное число !")
         return сardsNum = 0;
     } else {
         return сardsNum;
     }
 };
-
-function displayCards(){
-    productsForDisplay.forEach((product) => {
-    const productsClone = productsTemplate.content.cloneNode(true);
-    productsClone.querySelector('.card__category').textContent = product.category
-    productsClone.querySelector('.card__name').textContent = product.name
-    productsClone.querySelector('.card__description').textContent = product.description
-    productsClone.querySelector('.card__price-block span').textContent = product.price + ' ₽'
-    const imageElement = productsClone.querySelector('.card__image');
-    imageElement.src = product.image;
-
-    const compoundList = productsClone.querySelector('.compound__structure');
-    compoundList.innerHTML = '';
-
-    product.structure.forEach((item) => {
-        const li = document.createElement('li');
-        li.textContent = item;
-        compoundList.appendChild(li);
-    })
-    
-    productsList.appendChild(productsClone);
-})
-}
